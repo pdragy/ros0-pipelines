9a7bf832753afd6c7fde7958b8f8328bb1f3cb70
Paul Draghicescu <pdragy@gmail.com>
2025-12-27 15:56:13 -0800

    add depthcloud_encoder package

--- ros-noetic-depthcloud-encoder.groovy
+++ ros-noetic-depthcloud-encoder.groovy
@@ -1,4 +1,4 @@
 node {
-    git url: "https://github.com/RobotWebTools/depthcloud_encoder.git", branch: "devel"
+    git url: "https://github.com/RobotWebTools/depthcloud_encoder.git", branch: "develop"
     registerROS('osrf/ros:noetic-desktop-full', [], [])
 }
