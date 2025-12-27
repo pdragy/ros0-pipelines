39bdd7f135a9ce548cc165a109cbc120ae2446ea
Paul Draghicescu <pdragy@gmail.com>
2025-12-26 17:41:24 -0800

    wip noetic

--- ros-noetic-descartes.groovy
+++ ros-noetic-descartes.groovy
@@ -1,4 +1,4 @@
 node {
-    dir('src') { git url: "https://github.com/ros-industrial-consortium/descartes.git", branch: "melodic-devel" }
+    git url: "https://github.com/ros-industrial-consortium/descartes.git", branch: "melodic-devel"
     registerROS('osrf/ros:noetic-desktop-full', ['ros-noetic-moveit-ros-planning'], [])
 }
