907fb42927417efbf440416e134bbf366eae69f1
Paul Draghicescu <pdragy@gmail.com>
2025-12-26 19:00:24 -0800

    initial commits

--- ros-humble-drone-pose.groovy
+++ ros-humble-drone-pose.groovy
@@ -1,5 +1,4 @@
 node {
     git url: "https://github.com/Fixit-Davide/PX4-ROS2-Drone-Composable.git", branch: "Foxy_devel"
-    //registerROS('osrf/ros:humble-desktop-full', ['ros-humble-px4-msgs'], [])
-    sh "ls"
+    registerROS('osrf/ros:humble-desktop-full', ['ros-humble-px4-msgs'], [])
 }
