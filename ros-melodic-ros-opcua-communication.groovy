12e966182a0cc1b3a67c8a7c240a3d2b4f62f870
Paul Draghicescu <pdragy@gmail.com>
2025-12-27 18:37:58 -0800

    add package

--- ros-melodic-ros-opcua-communication.groovy
+++ ros-melodic-ros-opcua-communication.groovy
@@ -1,4 +1,5 @@
 node {
     git url: "https://github.com/iirob/ros_opcua_communication.git", branch: "kinetic-devel"
+    sh "git submodule update --init --recursive"
     registerROS('osrf/ros:melodic-desktop-full', [], [])
 }
