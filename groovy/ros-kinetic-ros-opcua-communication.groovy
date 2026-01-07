146413b808d471fed21964ca2a8fafddff6ac072
Paul Draghicescu <pdragy@gmail.com>
2026-01-07 11:43:37 -0800

    add ros-opcua-communication

---
+++ groovy/ros-kinetic-ros-opcua-communication.groovy
@@ -0,0 +1,5 @@
+node {
+    git url: "https://github.com/iirob/ros_opcua_communication.git", branch: "kinetic-devel"
+    sh "git submodule update --init --recursive"
+    registerROS('ros:kinetic-desktop-full', ["libmbedtls-dev"], [])
+}
