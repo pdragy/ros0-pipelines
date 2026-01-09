3b778f0c4ea566a7e9814a51696c4ea1565df723
Paul Draghicescu <pdragy@gmail.com>
2026-01-08 18:12:33 -0800

    add abb_egm_rws_managers

---
+++ groovy/ros-noetic-abb-egm-rws-managers.groovy
@@ -0,0 +1,4 @@
+node {
+    git url: "https://github.com/ros-industrial/abb_egm_rws_managers.git", branch: "master"
+    registerROS('ros:noetic-desktop-full', ['ros-noetic-abb-libegm', 'ros-noetic-abb-librws'], [])
+}
