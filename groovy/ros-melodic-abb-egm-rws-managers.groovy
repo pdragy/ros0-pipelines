570642c551dfab48def217f019b7b3572ccf3de9
Paul Draghicescu <pdragy@gmail.com>
2026-01-08 18:12:33 -0800

    add abb_egm_rws_managers

---
+++ groovy/ros-melodic-abb-egm-rws-managers.groovy
@@ -0,0 +1,4 @@
+node {
+    git url: "https://github.com/ros-industrial/abb_egm_rws_managers.git", branch: "master"
+    registerROS('ros:melodic-desktop-full', ['ros-melodic-abb-libegm', 'ros-melodic-abb-librws'], [])
+}
