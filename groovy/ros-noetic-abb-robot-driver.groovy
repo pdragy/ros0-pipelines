bcf33dfa316e18f5f46fe14950b1600e385eafd4
Paul Draghicescu <pdragy@gmail.com>
2026-01-08 21:28:47 -0800

    add abb-robot-driver

---
+++ groovy/ros-noetic-abb-robot-driver.groovy
@@ -0,0 +1,12 @@
+node {
+    git url: "https://github.com/ros-industrial/abb_robot_driver.git", branch: "master"
+    registerROS('ros:noetic-desktop-full', [
+        'ros-noetic-abb-egm-msgs',
+        'ros-noetic-abb-egm-rws-managers',
+        'ros-noetic-abb-libegm',
+        'ros-noetic-abb-librws',
+        'ros-noetic-abb-rapid-msgs',
+        'ros-noetic-abb-rapid-sm-addin-msgs',
+        'ros-noetic-abb-robot-msgs'
+    ], [])
+}
