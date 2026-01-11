44773355505028dd1075d016deb331ae55722a1e
Paul Draghicescu <pdragy@gmail.com>
2026-01-10 22:39:34 -0800

    add indigo ros-opcua-communication

---
+++ groovy/ros-indigo-ros-opcua-communication.groovy
@@ -0,0 +1,5 @@
+node {
+    git url: "https://github.com/iirob/ros_opcua_communication.git", branch: "indigo-devel"
+    sh "git submodule update --init --recursive"
+    registerROS('ros:indigo-desktop-full', [], [])
+}
