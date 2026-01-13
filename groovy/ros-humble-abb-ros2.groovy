889034713b130f00dbdbcdc97a9f23d40da480d3
Paul Draghicescu <pdragy@gmail.com>
2026-01-13 11:55:34 -0800

    fixes abb-ros2

--- groovy/ros-humble-abb-ros2.groovy
+++ groovy/ros-humble-abb-ros2.groovy
@@ -1,8 +1,9 @@
 node {
     git url: "https://github.com/PickNikRobotics/abb_ros2.git", branch: "humble"
-    registerROS('ros:jazzy-desktop-full', [
+    registerROS('ros:humble-desktop-full', [
         'ros-humble-abb-egm-rws-managers',
         'ros-humble-abb-ros2-msgs',
+        'ros-humble-controller-manager',
         'ros-humble-hardware-interface',
         'ros-humble-ros-testing'
     ], [])
