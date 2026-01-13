8ecd9911094696450f42614eb9fcc3f87270f0d7
Paul Draghicescu <pdragy@gmail.com>
2026-01-12 23:52:25 -0800

    add abb-ros2 deps

---
+++ groovy/ros-jazzy-abb-egm-rws-managers.groovy
@@ -0,0 +1,7 @@
+node {
+    git url: "https://github.com/ros-industrial/abb_egm_rws_managers.git", branch: "master"
+    registerROS('ros:jazzy-desktop-full',
+        ['ros-jazzy-abb-libegm', 'ros-jazzy-abb-librws'],
+        ['ros-jazzy-abb-libegm', 'ros-jazzy-abb-librws']
+    )
+}
