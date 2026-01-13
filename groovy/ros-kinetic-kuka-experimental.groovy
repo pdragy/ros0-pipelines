5e1d9d8e67691889c80bd7f615b258b4e54657fb
Paul Draghicescu <pdragy@gmail.com>
2026-01-12 17:31:14 -0800

    add kuka-experimental

---
+++ groovy/ros-kinetic-kuka-experimental.groovy
@@ -0,0 +1,9 @@
+node {
+    git url: "https://github.com/ros-industrial/kuka_experimental.git", branch: "melodic-devel"
+    registerROS('ros:kinetic-desktop-full', [
+        'ros-kinetic-controller-manager',
+        'ros-kinetic-industrial-robot-client',
+        'ros-kinetic-joint-limits-interface',
+        'ros-kinetic-realtime-tools'
+    ], [])
+}
---
+++ groovy/ros-noetic-kuka-experimental.groovy
@@ -0,0 +1,8 @@
+node {
+    git url: "https://github.com/ros-industrial/kuka_experimental.git", branch: "melodic-devel"
+    registerROS('ros:noetic-desktop-full', [
+        'ros-noetic-controller-manager',
+        'ros-noetic-industrial-robot-client',
+        'ros-noetic-joint-limits-interface'
+    ], [])
+}
