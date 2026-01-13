47d9c376a576becb21678580ae3e5fe17ee0747c
Paul Draghicescu <pdragy@gmail.com>
2026-01-12 17:31:14 -0800

    add kuka-experimental

---
+++ groovy/ros-indigo-kuka-experimental.groovy
@@ -0,0 +1,8 @@
+node {
+    git url: "https://github.com/ros-industrial/kuka_experimental.git", branch: "indigo-devel"
+    registerROS('ros:indigo-desktop-full', [
+        'ros-indigo-controller-manager',
+        'ros-indigo-industrial-robot-client',
+        'ros-indigo-joint-limits-interface'
+    ], [])
+}
---
+++ groovy/ros-melodic-kuka-experimental.groovy
@@ -0,0 +1,8 @@
+node {
+    git url: "https://github.com/ros-industrial/kuka_experimental.git", branch: "melodic-devel"
+    registerROS('ros:melodic-desktop-full', [
+        'ros-melodic-controller-manager',
+        'ros-melodic-industrial-robot-client',
+        'ros-melodic-joint-limits-interface'
+    ], [])
+}
