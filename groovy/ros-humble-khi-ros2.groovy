d51621cd12b8a1a83b02bdba83a74ec7b9c224b3
Paul Draghicescu <pdragy@gmail.com>
2026-01-17 12:22:32 -0800

    khi-ros2 control node run deps

--- groovy/ros-humble-khi-ros2.groovy
+++ groovy/ros-humble-khi-ros2.groovy
@@ -3,5 +3,10 @@ node {
     registerROS('ros:humble-ros-base', [
         'ros-humble-hardware-interface',
         'ros-humble-moveit-common'
-    ], [])
+    ], [
+        'ros-humble-controller-manager',
+        'ros-humble-force-torque-sensor-broadcaster',
+        'ros-humble-joint-state-broadcaster',
+        'ros-humble-joint-trajectory-controller'
+    ])
 }
