0326931cea964ee9ae078f65ff92f22e88cde222
Paul Draghicescu <pdragy@gmail.com>
2026-01-18 14:12:45 -0800

    add missing lbr-stack build deps

--- groovy/ros-humble-lbr-fri-ros2-stack.groovy
+++ groovy/ros-humble-lbr-fri-ros2-stack.groovy
@@ -1,8 +1,11 @@
 node {
     git url: 'https://github.com/lbr-stack/lbr_fri_ros2_stack.git', branch: 'humble'
     registerROS('ros:humble-ros-base', [
+        'ros-humble-controller-interface',
         'ros-humble-fri-client-sdk',
+        'ros-humble-kinematics-interface',
         'ros-humble-lbr-fri-idl',
-        'ros-humble-moveit-ros-planning-interface'
+        'ros-humble-moveit-ros-planning-interface',
+        'ros-humble-realtime-tools'
     ], [])
 }
