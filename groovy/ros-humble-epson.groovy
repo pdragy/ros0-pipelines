5998ecac1ce7de4bf0e00b2c2bc5db2890f08852
Paul Draghicescu <pdragy@gmail.com>
2026-01-19 10:47:47 -0800

    add epson

--- groovy/ros-humble-epson.groovy
+++ groovy/ros-humble-epson.groovy
@@ -2,6 +2,7 @@ node {
     git url: 'https://github.com/Epson-Robots/epson-robot-ros2.git', branch: 'main'
     registerROS('ros:humble-ros-base', [
         'ros-humble-controller-manager',
-        'ros-humble-joint-trajectory-controller'
+        'ros-humble-joint-trajectory-controller',
+        'ros-humble-moveit-ros-planning-interface'
     ], [])
 }
