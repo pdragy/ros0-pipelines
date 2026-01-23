5befe35ee0a1f0f7a6518d9695e30288cf24c7be
Paul Draghicescu <pdragy@gmail.com>
2026-01-23 12:05:33 -0800

    fix humble tests

--- groovy/ros-humble-crane-x7-description.groovy
+++ groovy/ros-humble-crane-x7-description.groovy
@@ -1,4 +1,4 @@
 node {
     git url: "https://github.com/rt-net/crane_x7_description.git", branch: "ros2"
-    registerROS('ros:humble-ros-base', ['ros-humble-xacro'], [])
+    registerROS('ros:humble-ros-base', ['ros-humble-xacro', 'ros-humble-realsense2-description'], [])
 }
