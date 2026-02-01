8ac536517674acccb2b86df9974f42fa58372d7d
Paul Draghicescu <pdragy@gmail.com>
2026-01-31 17:33:55 -0800

    wip rosbag2-snapshot

--- groovy/ros-humble-rosbag2-snapshot.groovy
+++ groovy/ros-humble-rosbag2-snapshot.groovy
@@ -1,4 +1,4 @@
 node {
-    git url: 'https://github.com/angsa-robotics/rosbag2_snapshot.git', branch: 'main'
+    git url: 'https://github.com/gennartan/rosbag2_snapshot.git', branch: 'humble-devel'
     registerROS('ros:humble-ros-base', [], [])
 }
