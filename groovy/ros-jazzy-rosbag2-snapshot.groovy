c00ccace6b49ef6bd6294ac3cea842526d8cd738
Paul Draghicescu <pdragy@gmail.com>
2026-01-31 17:19:39 -0800

    fix jazzy rosbag2-snapshot

--- groovy/ros-jazzy-rosbag2-snapshot.groovy
+++ groovy/ros-jazzy-rosbag2-snapshot.groovy
@@ -1,4 +1,4 @@
 node {
-    git url: 'https://github.com/gaia-platform/rosbag2_snapshot.git', branch: 'main'
+    git url: 'https://github.com/angsa-robotics/rosbag2_snapshot.git', branch: 'main'
     registerROS('ros:jazzy-ros-base', [], [])
 }
